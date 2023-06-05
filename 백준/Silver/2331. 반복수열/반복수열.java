a8e0c5db280874dfbf85684a44830901973c5467
jhwan <52308544+JeongHwan95@users.noreply.github.com>
2023-06-05 18:16:47 +0900

    [Silver IV] Title: 반복수열, Time: 120 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Silver/2331. 반복수열/반복수열.java
+++ 백준/Silver/2331. 반복수열/반복수열.java
@@ -1,70 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
-
 	
-	public static void  main(String[] args) throws IOException{
+	static int power;
+//	static int[] check = new int[3333333];
+    // I think using HashMap is more adequate that arr in this quiz
+	static Map<Integer, Integer> map = new HashMap<>();
+	
+	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		// Receive the values
-		String in = br.readLine();
-		String[] a = in.split(" ");
-		int multiple = Integer.parseInt(a[1]) ;
+		int element = Integer.parseInt(st.nextToken());
+		power = Integer.parseInt(st.nextToken());
 		
-		// Make list for adding value
-		List<Integer> list = new ArrayList<Integer>();
-		list.add(Integer.parseInt(a[0]));
-		
-		// Get a element through while syntax one by one 
-		// and check Whether find a repetitive element or not
-		int answer = 0;
-		while(true) {
-			
-			int now = list.get(list.size()-1);
-			
-			
-			int value = 0;
-			while( now > 0) {
-				
-				int next = 1;
-				for(int i=0; i<multiple; i++) {
-					next*=(now%10);
-				} // for
-				
-				value += next;
-				now /= 10;
-			} // while
-			
-			// If a repetitive element exist, flag get a 'true' value through circulating list
-			boolean flag = false;
-			for(int j = 0; j<list.size(); j++ ) {
-				
-				if(list.get(j) == value) {
-					answer = j;
-					flag = true;
-					break;
-				} // if
-			} // for
-			
-			if(flag == true) {
-				break;
-			}
-			
-			list.add(value);
-			
-		} // while 
-		
-		
-		System.out.println(answer);
-		
-//		for(int i : list) {
-//			System.out.print(i + " ");
-//		} // for
+//		check[element] = 1;
+		map.put(element, 1);
+		System.out.println(dfs(element, 1));
 		
 	} // main
+	
+	public static int getNext(int element) {
+		
+		int next = 0;
+		while(element>0) {
+			
+			next += Math.pow( element%10, power);
+			
+			element /= 10;
+		} // while
+		
+//		System.out.print(next + " ");
+		return next;
+		
+	} // next
+	
+	// Get nextNode using getNext() function 
+	// If the value already exist in check[nextNode], return check[nextNode] -1
+	// If not, call oneself
+	public static int dfs(int element, int cnt) {
+		
+		
+		int next = getNext(element);
+		
+		
+		if(map.containsKey(next)) {
+				return map.get(next) - 1;
+			}
+		else {
+			map.put(next, ++cnt);
+			
+			return dfs(next, cnt);
+		} // if - else	
+	} // dfs(int, int)
 	
 } // end class
