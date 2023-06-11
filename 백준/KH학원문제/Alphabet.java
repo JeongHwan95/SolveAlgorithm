a883305b0157ab9dedd3b8f493f00a51ba3f48ec
jhwan <52308544+JeongHwan95@users.noreply.github.com>
2023-06-11 18:24:31 +0900

    Alphabet.java

---
+++ 백준/KH학원문제/Alphabet.java
@@ -0,0 +1,10 @@
+public class Alphabet {
+	char alphabet;
+	int length;
+
+	//하나의 객체는 알파벳과 길이(시간)을 저장할 수 있다.
+	Alphabet(char alphabet, int length){
+		this.alphabet = alphabet;
+		this.length = length;
+	}//Constructor(char alphabet, int length)
+}
