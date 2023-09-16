a14304ea7f7a91bd620f7cd72348e8b2b1ab7fc9
jhwan <52308544+JeongHwan95@users.noreply.github.com>
2023-09-17 01:01:04 +0900

    [Silver IV] Title: 반복수열, Time: 76 ms, Memory: 12648 KB -BaekjoonHub

--- 백준/Silver/2331. 반복수열/반복수열.java
+++ 백준/Silver/2331. 반복수열/반복수열.java
@@ -1,63 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
+// 2331번
 public class Main {
 	
-	static int power;
-//	static int[] check = new int[3333333];
-    // I think using HashMap is more adequate that arr in this quiz
-	static Map<Integer, Integer> map = new HashMap<>();
+	static int[] checking = new int[300000];
+	static int A;
+	static int P;
+	static int count = 0;
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int element = Integer.parseInt(st.nextToken());
-		power = Integer.parseInt(st.nextToken());
+		A = Integer.parseInt(st.nextToken());
+		P = Integer.parseInt(st.nextToken());
+		int index = 0;
+		checking[A] = ++index;
+		count++;
 		
-//		check[element] = 1;
-		map.put(element, 1);
-		System.out.println(dfs(element, 1));
-		
-	} // main
-	
-	public static int getNext(int element) {
-		
-		int next = 0;
-		while(element>0) {
+		int b = A;
+		do {
 			
-			next += Math.pow( element%10, power);
 			
-			element /= 10;
-		} // while
-		
-//		System.out.print(next + " ");
-		return next;
-		
-	} // next
-	
-	// Get nextNode using getNext() function 
-	// If the value already exist in check[nextNode], return check[nextNode] -1
-	// If not, call oneself
-	public static int dfs(int element, int cnt) {
-		
-		
-		int next = getNext(element);
-		
-		
-		if(map.containsKey(next)) {
-				return map.get(next) - 1;
+			int sum = 0;
+			while(b%10!=0 || b>9) {
+				sum += Math.pow(b%10, P);
+				
+				b /= 10;
 			}
-		else {
-			map.put(next, ++cnt);
 			
-			return dfs(next, cnt);
-		} // if - else	
-	} // dfs(int, int)
+			b = sum;
+			
+			if(checking[b]!=0) {
+				index = checking[b];
+				break;
+			}
+			else{
+				checking[b] = ++index;
+				count++;
+//				System.out.print(b+" ");
+			}
+		} while(true);
+		
+		
+		System.out.println(--index);
+	} // main
 	
 } // end class
