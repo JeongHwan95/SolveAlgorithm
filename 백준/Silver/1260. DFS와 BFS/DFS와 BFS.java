2f62c2fdd5362f489335cf020687a6ba25641144
jhwan <52308544+JeongHwan95@users.noreply.github.com>
2023-09-16 16:14:14 +0900

    [Silver II] Title: DFS와 BFS, Time: 208 ms, Memory: 21192 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,102 +1,95 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
+
+// 1260번
 public class Main {
 	
-	static StringBuffer sb = new StringBuffer();
-	static boolean[] arrayForChecking;
-	static boolean[][] graph;
 	static int N;
 	static int M;
 	static int V;
 	
-	public static void main(String[] args) throws IOException {
+	static int map[][];
+	static boolean visited[];
+	static StringBuffer sb;
+	
+	public static void main(String[] args) throws IOException{ 
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuffer();
 		
-		String line = br.readLine();
-		String[] information = line.split(" ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		V = Integer.parseInt(st.nextToken());
 		
-		N = Integer.parseInt(information[0]);
-		M = Integer.parseInt(information[1]);
-		V = Integer.parseInt(information[2]);
-		
-		
-		// Create a dataStructure for receving graph 
-		graph = new boolean[N+1][N+1];
+		map = new int[N+1][N+1];
 		
 		
-		// Store values of a graph into a dataStructure
 		for(int i=1; i<M+1; i++) {
-			line = br.readLine();
-			String[] xy = line.split(" ");
 			
-			int x = Integer.parseInt(xy[0]);
-			int y = Integer.parseInt(xy[1]);
+			st = new StringTokenizer(br.readLine());
 			
-			graph[x][y] = graph[y][x] = true;
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			map[x][y] = map[y][x] = 1;
 		} // for
 		
+		visited = new boolean[N+1];
+		visited[V] = true;
+		sb.append(V+" ");
+		dfs(V);
 		
-		// This block exist for DFS
-		{
-			// Create an array for checking Whether to be through vertex or not
-			arrayForChecking = new boolean[N+1];
-					
-			// Execute DFS and print it
-			Main.dfs(V);
-			System.out.println(sb.toString());
-		}
+		visited = new boolean[N+1];
+		sb.append("\n");
+		bfs(V);
 		
-		// This block exist for BFS
-		{
-			// Initialize StringBuffer and array for checking
-			sb = new StringBuffer();
-			arrayForChecking = new boolean[N+1];
+		System.out.println(sb.toString());
+		
+	} // end main
+	
+	
+	public static void dfs(int v) {
+		
+		
+		for(int i=1; i<=N; i++) {
 			
-			Main.bfs(V);
-			System.out.println(sb.toString());
-		}
-	} // main
-
-	
-	
-	public static void dfs(int start) {
-		
-		sb.append(start + " ");
-		arrayForChecking[start] = true;
-		
-		for(int i=1; i<N+1; i++) {
-			if(arrayForChecking[i]==false && graph[i][start]==true) {
-
+			if(!visited[i] && map[i][v]==1) {
+				visited[i]=true;
+				sb.append(i+" ");
 				dfs(i);
 			} // if
 		} // for
-	} // dfs()
+	} // dfs
 	
-	public static void bfs(int start) {
+	
+	public static void bfs(int v) {
 		
-		Queue<Integer> queue = new LinkedList<Integer>();
-		queue.add(start);
-		arrayForChecking[start] = true;
-		sb.append(start+" ");
+		visited[v] = true;
+		Queue<Integer> queue = new ArrayDeque<>();
+		queue.add(v);
 		
 		
-			
 		while(!queue.isEmpty()) {
-			int currentVertex = queue.poll();
 			
-			for(int i=1; i<N+1; i++) {
+			int x = queue.poll();
+			sb.append(x+" ");
+			
+			for(int y=1; y<N+1; y++) {
 				
-				if(graph[currentVertex][i]==true && arrayForChecking[i]==false) {
-					queue.add(i);
-					arrayForChecking[i] = true;
-					sb.append(i+" ");
+				if(!visited[y] && map[x][y] == 1) {
+					
+					visited[y] = true;
+					queue.add(y);
+					
 				} // if
 			} // for
-		} // while	
-	}// bfs
+		} // while
+		
+	} // bfs
 } // end class
