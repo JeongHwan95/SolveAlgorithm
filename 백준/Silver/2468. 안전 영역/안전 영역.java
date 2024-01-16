5802fef77c4556d343ffd7970a94010f02e7e2b1
jhwan <52308544+JeongHwan95@users.noreply.github.com>
2024-01-16 17:21:52 +0900

    [Silver I] Title: 안전 영역, Time: 304 ms, Memory: 35028 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,86 +1,93 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
-// Complete
-public class Main {
-
+//2468번
+// 풀었을 때 기쁨은 얼마 안된다.
+// 시간의 대다수는 내가 풀어가는 과정임으로,
+// 풀어가는 과정에서 기쁨과 재미를 찾아야 한다.
+public class Main{
+	
+	static int N;
 	static int[][] map;
-	static boolean[][] check;
-	static int num;
-	static int[] dx = { -1, 0, 1, 0};
-	static int[] dy = { 0, -1, 0, 1};
-	static int length;
+	static int count;
+	static boolean flag;
+	static boolean[][] visited;
+	static int[] dx = {-1, 0, 1, 0};
+	static int[] dy = {0, -1, 0, 1};
 	
 	public static void main(String[] args) throws IOException{
-		
-		// Receive values
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String n = br.readLine();
-		length = Integer.parseInt(n);
-
-		// Put elements into map
-		map = new int[length][length];
-		check = new boolean[length][length];
-		for(int i=0; i<length; i++) {
+		
+		N = Integer.parseInt(br.readLine());
+		
+		
+		map = new int[N][N];
+		for(int i=0; i<N; i++) {
 			
-			String line = br.readLine();
-			String[] arrForLine = line.split(" ");
-			for(int j=0; j<length; j++) {
-				map[i][j] = Integer.parseInt(arrForLine[j]); 
-			} // for
-		} // for
-	
+			String[] input = br.readLine().split(" ");
+			for(int j=0; j<N; j++) {
+				map[i][j] = Integer.parseInt(input[j]);
+			}
+		}
 		
 		int answer = 0;
-		for(int rain=0; rain<100; rain++) {
+		for(int rain=1; rain<=100; rain++) {
 			
-			// When logic restart, these have to be initialize.
-			num = 0;
-			check = new boolean[length][length];
-			
-			for(int x=0; x<length; x++) {
-				for(int y=0; y<length; y++) {
-					
-					if(map[x][y]>rain && !check[x][y]) {
-						check[x][y] = true;
-						num++;
-						dfs(x, y, rain);
-						
-					} // if
-				} // for
+			count = 0;
+			visited = new boolean[N][N];
+			for(int i=0; i<N; i++) {
 				
-
-			} // for
-			
-//			if(answer < num ) {
-//				System.out.println("num, rain: " + num + ", "+ rain);
-//			} // if
-//			
-			answer = answer > num ? answer : num;
-		} // for 
-		
+				for(int j=0; j<N; j++) {
+					flag = false;
+					
+					bfs(i, j, rain);
+					
+					if(flag) {
+						count++;
+					}
+				}
+			}
+			answer = Math.max(answer, count);
+		}
 		System.out.println(answer);
-	} // end main
-
+	}
 	
-	public static void dfs(int x, int y, int rain) {
+	
+	public static void bfs(int x, int y, int rain) {
+		if(map[x][y]<rain || visited[x][y]) {
+			return;
+		}
 		
+		flag = true;
+		visited[x][y] = true;
+		Queue<Integer []> queue = new ArrayDeque<>();
+		Integer[] add = {x, y};
+		queue.add(add);
 		
-		for(int i=0; i<4; i++) {
-			int nextX = dx[i] + x;
-			int nextY = dy[i] + y;
+		while(!queue.isEmpty()) {
 			
-			if(nextX <= -1 || nextX >= length || nextY <= -1 || nextY >= length) 
-				continue;
+			Integer[] curV = queue.poll();
 			
-			if(map[nextX][nextY] > rain && !check[nextX][nextY]) {
-				check[nextX][nextY] = true;
-				dfs(nextX, nextY, rain);
-			} // if
-		} // for
+			for(int i=0; i<4; i++) {
+				
+				int nextX = curV[0] + dx[i];
+				int nextY = curV[1] + dy[i];
+				
+				if(nextX<0 || nextY<0 || nextX>=N || nextY>=N) {
+					continue;
+				}
+				
+				if(map[nextX][nextY]>=rain && !visited[nextX][nextY]) {
+					visited[nextX][nextY] = true;
+					Integer[] addV = {nextX, nextY};
+					queue.add(addV);
+				}
+			}
+			
+		}
 		
-		
-		
-	} // end dfs(x, y, rain)
+	}
 } // end class
