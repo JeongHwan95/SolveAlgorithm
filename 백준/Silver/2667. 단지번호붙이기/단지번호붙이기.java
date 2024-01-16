85b72507ce2a3659095a7db665607c0b8fd8a511
jhwan <52308544+JeongHwan95@users.noreply.github.com>
2024-01-16 17:57:55 +0900

    [Silver I] Title: 단지번호붙이기, Time: 232 ms, Memory: 18320 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -5,91 +5,82 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Main {
+//2667번
+// 풀었을 때 기쁨은 얼마 안된다.
+// 시간의 대다수는 내가 풀어가는 과정임으로,
+// 풀어가는 과정에서 기쁨과 재미를 찾아야 한다.
+public class Main{
 	
-	static int length;
+	static int N;
 	static int[][] map;
-	static boolean[][] check;
+	static int[] dx = {-1, 0, 1, 0};
+	static int[] dy = {0, -1, 0, 1};
+	static boolean flag;
 	static int count;
-	static List<Integer> countList = new ArrayList<>();
-	static int[] dx = { -1, 0, 1, 0 };
-	static int[] dy = { 0, -1, 0, 1 };
-	
+	static boolean[][] visited;
 	
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		List<Integer> answer = new ArrayList<>();
 		
-		String len = br.readLine();
-		length = Integer.parseInt(len);
-		map = new int[length][length];
-		check = new boolean[length][length];
-		
-		// Complete to input elements into map 
-		for(int i=0; i<length; i++) {
+		map = new int[N][N];
+		for(int i=0; i<N; i++) {
 			
-			String line = br.readLine();
-			for(int j=0; j<length; j++) {
+			String s = br.readLine();
+			for(int j=0; j<N; j++) {
 				
-				if(line.charAt(j) == '0') {
-					map[i][j] = 0;
+				map[i][j]=s.charAt(j)-'0';
+			}
+		}
+		
+		
+		visited = new boolean[N][N];
+		for(int i=0; i<N; i++) {
+			
+			for(int j=0; j<N; j++) {
+				flag = false;
+				count=0;
+				dfs(i, j);
+				
+				if(flag) {
+					answer.add(count);
 				}
-				else {
-					map[i][j] = 1;
-				} // if - else
-			} // for
-		} // for
-
+			}
+		}
 		
-		// Get counts as searching map through DFS
-		for(int row = 0; row < length; row++) {
-			for(int column = 0; column < length; column++) {
-				
-				count = 0;
-				if(map[row][column] == 1 && check[row][column] == false) {
-					count++;
-					dfs(row, column);
-				} // if
-				
-				// Get a count if count is not 0
-				if(count != 0) {
-//					System.out.println("row, column, count : " + row + ", " + column + ", " + count);
-					countList.add(count);
-				} // if
-			} //for
-		} // for
+		System.out.println(answer.size());
+		Collections.sort(answer);
+		answer.forEach(i -> {
+			System.out.println(i);
+		});
 		
-		
-		// print answers
-		Collections.sort(countList);
-		System.out.println(countList.size());
-		for(int answer : countList) {
-			System.out.println(answer);
-		} // for
-		
-		br.close();
-		
-	} // end main
+	}
 	
-	
-	public static void dfs(int row, int column) {
-//		System.out.println("dfs( " +row +", "+column + " )");
-		check[row][column] = true;
+	public static void dfs(int x, int y) {
+		
+		if(visited[x][y] || map[x][y]==0) {
+			return;
+		}
+		
+		visited[x][y] = true;
+		flag = true;
+		count++;
 		
 		for(int i=0; i<4; i++) {
 			
-			int nextX = row + dx[i];
-			int nextY = column + dy[i];
+			int nextX = x + dx[i];
+			int nextY = y + dy[i];
 			
-			
-			if(nextX >= length || nextY >=length || nextX <= -1 || nextY <=-1) 
+			if(nextX<0 || nextY<0 || nextX>=N || nextY>=N) {
 				continue;
+			}
 			
-			if(map[nextX][nextY] == 1 && check[nextX][nextY] == false ) {
-				count++;
+			if(map[nextX][nextY]==1 && !visited[nextX][nextY]) {
 				dfs(nextX, nextY);
-			} // if
-		} // for
-	} // dfs
-	
+			}
+		}
+		
+	}
 } // end class
