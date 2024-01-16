0f6e5ca63d06fcfb7a8614f6133784f0ec538b18
jhwan <52308544+JeongHwan95@users.noreply.github.com>
2024-01-16 18:42:07 +0900

    [Silver I] Title: 단지번호붙이기, Time: 232 ms, Memory: 18256 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,9 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Queue;
 
 //2667번
 // 풀었을 때 기쁨은 얼마 안된다.
@@ -42,7 +44,8 @@ public class Main{
 			for(int j=0; j<N; j++) {
 				flag = false;
 				count=0;
-				dfs(i, j);
+//				dfs(i, j);
+				bfs(i, j);
 				
 				if(flag) {
 					answer.add(count);
@@ -57,6 +60,46 @@ public class Main{
 		});
 		
 	}
+	
+	
+	public static void bfs(int x, int y) {
+		if(map[x][y]==0 || visited[x][y]) {
+			return;
+		}
+		
+		count++;
+		flag=true;
+		visited[x][y] = true;
+		Integer[] add = {x, y};
+		Queue<Integer[]> queue = new ArrayDeque<>();
+		queue.add(add);
+		
+		while(!queue.isEmpty()) {
+			
+			Integer[] curV = queue.poll();
+			
+			for(int i=0; i<4; i++) {
+			
+				int nextX = curV[0] + dx[i];
+				int nextY = curV[1] + dy[i];
+					
+				if(nextX<0 || nextY<0 || nextX>=N || nextY>=N) {
+					continue;
+				}
+				
+				if(!visited[nextX][nextY] && map[nextX][nextY]==1) {
+					
+					Integer[] newV = {nextX, nextY};
+					queue.add(newV);
+					visited[nextX][nextY] = true;
+					count++;
+				}
+			}
+			
+		}
+		
+	}
+	
 	
 	public static void dfs(int x, int y) {
 		
