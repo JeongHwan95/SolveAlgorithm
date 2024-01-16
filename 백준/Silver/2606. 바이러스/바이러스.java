390fb9470f044720fd211817a0f04e43355b4c6d
jhwan <52308544+JeongHwan95@users.noreply.github.com>
2024-01-16 14:23:11 +0900

    [Silver III] Title: 바이러스, Time: 76 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,69 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main {
-
-	static int N;
-	static int K;
-	static boolean[] visited;
+//2667번
+public class Main{
+	
+	static int N, M;
 	static int[][] map;
+	static boolean[] visited;
+	static int count = 0;
 	
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st; 
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
 		
-		// 초기화 및 입력 과정
-		{	
-			N = Integer.parseInt(br.readLine());
-			K = Integer.parseInt(br.readLine());
+		map = new int[N+1][N+1];
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
 			
-			visited = new boolean[N+1];
-			visited[1] = true;
-			map = new int[N+1][N+1];
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
 			
-			for(int i=0; i<K; i++) {
-				
-				String line = br.readLine();
-				String[] arrForToken = line.split(" ");
-				
-				int v1 = Integer.parseInt(arrForToken[0]);
-				int v2 = Integer.parseInt(arrForToken[1]);
-				
-				map[v1][v2] = map[v2][v1] = 1;
-			} // for
-		} // finish the block
+			map[x][y] = map[y][x] = 1;
+		}
 		
-		// bfs 
-		int count = bfs(1);
-		
-		// 답을 출력
-		System.out.println(count);
-	} // end main
+		visited = new boolean[N+1];
+		dfs(1);
+		System.out.println(count-1);
+	}
 	
-	public static int bfs(int startPoint) {
+	public static void dfs(int v) {
 		
-		int answer = 0;
-		Queue<Integer> queue = new ArrayDeque<>();
-		queue.add(startPoint);
+		visited[v] = true;
+		count++;
 		
+		for(int i=1; i<=N; i++) {
+			if(!visited[i] && map[v][i]==1) {
+				dfs(i);
+			}
+		}
 		
-		while(!queue.isEmpty()) {
-			
-			int intCurrent = queue.poll();
-			for(int i=1; i<=N; i++) {
-				
-				if(map[intCurrent][i] == 1 && !visited[i]) {
-					
-					visited[i] = true;
-					answer++;
-					queue.add(i);
-				} // if
-			} // for
-		} // while
-		
-		return answer;
-	} // bfs
+	}
 } // end class
